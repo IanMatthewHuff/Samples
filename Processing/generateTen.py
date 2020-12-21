d77f5658d2c26e8ef6015e647e4272e440bc87ee
Ian Huff <ian.huff@gmail.com>
2020-12-21 11:42:05 -0800

    starting animated py work

---
+++ Processing/generateTen.py
@@ -0,0 +1,11 @@
+# Test here to generate 10 unique images in a directory
+
+# noLoop to prevent auto drawing, and redraw to run code in draw a single time
+def setup():
+    size(100, 100)
+    noLoop()
+
+
+def draw():
+    background(255)
+    fill(0)
